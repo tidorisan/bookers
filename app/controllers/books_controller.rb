4c43da83d714e0c54ab0a8f36f3b960c53437099
vagrant <vagrant@localhost.localdomain>
2020-06-27 23:26:39 +0000

    [update]show.html.erb

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,30 +1,30 @@
 class BooksController < ApplicationController
 	def top
-
 	end
 
 	def index
 		@book = Book.new
-		# db作成後　dateある状態
 		@books = Book.all
 	end
 
 	def create
 		book = Book.new(book_params)
-		book.save
-		# showがまだできていないので
-		redirect_to books_path
+		if  book.save
+			redirect_to book_path(book.id)
+		else
+			render :index
+		end
 	end
 
 	def show
-		
+		@book = Book.find(params[:id])
 	end
 
 	def edit
-		
 	end
 
 	private
+
 	def book_params
 		params.require(:book).permit(:title, :body)
 	end
